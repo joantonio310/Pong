26f8435931a431437f16f9c86fbc7d3db815c48a
Antonio <joantonio310@outlook.com>
2017-04-09 18:49:27 -0500

    Poniendo la textura y la musica

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(1024, 765), "Pong");
+    // Load a sprite to display
+    sf::Texture texture;
+    if (!texture.loadFromFile("soccerField.jpeg"))
+        return EXIT_FAILURE;
+    sf::Sprite sprite(texture);
+
+    // Load a music to play
+    sf::Music music;
+    if (!music.openFromFile("music.wav"))
+        return EXIT_FAILURE;
+    // Play the music
+    music.play();
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Draw the sprite
+        window.draw(sprite);
+        // Update the window
+        window.display();
+    }
+    return EXIT_SUCCESS;
+}
