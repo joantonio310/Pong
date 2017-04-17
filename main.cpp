1f4a9c985fb387df92c587d47376acb6af8a449b
Antonio <joantonio310@outlook.com>
2017-04-17 01:14:53 -0500

    Implementación rebote paleta derecha

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <stdlib.h>
+#include <cmath>
 
 int main()
 {
 	sf::Vector2f paddleSize(25, 100);
+	float ballAngle = 0.f; // to be changed later
+	bool right = true;
+	const float pi = 3.14159f;
 
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(1024, 765), "Pong");
@@ -80,6 +84,39 @@ int main()
         	rightPaddle.getPosition().y < 660)
         {
         	rightPaddle.move(0.f, 1.f);
+        }
+
+        //move the ball
+        if(right)
+        	ball.move(std::cos(ballAngle), std::sin(ballAngle));
+        else
+        	ball.move(-std::cos(ballAngle), -std::sin(ballAngle));
+
+        if(ball.getPosition().x > 900){
+        	//right = false;
+	        if(rightPaddle.getPosition().y - ball.getPosition().y  >= 10 && rightPaddle.getPosition().y - ball.getPosition().y  <= 30){
+	        	printf("En medio\n");
+	        	ballAngle = 0.f;
+	        }
+	    	else if(rightPaddle.getPosition().y - ball.getPosition().y  >= 31 && rightPaddle.getPosition().y - ball.getPosition().y  <= 50){
+	        	printf("Arriba\n");
+	        	ballAngle = -(5*pi)/6;
+	    	}
+	        else if(rightPaddle.getPosition().y - ball.getPosition().y  >= -10 && rightPaddle.getPosition().y - ball.getPosition().y  <= 9){
+	        	printf("Abajo\n");
+	        	ballAngle = (5*pi)/6;
+	        }
+	        else if(rightPaddle.getPosition().y - ball.getPosition().y  >= 51 && rightPaddle.getPosition().y - ball.getPosition().y  <= 70){
+	        	printf("Hasta arriba\n");
+	        	ballAngle = -(2*pi)/3;
+	        }
+	        else if(rightPaddle.getPosition().y - ball.getPosition().y  >= -30 && rightPaddle.getPosition().y - ball.getPosition().y  <= -11){
+	        	printf("Hasta abajo\n");
+	        	ballAngle = (2*pi)/3;
+	        }
+	        else{
+	        	printf("Nop\n");
+	        }
         }
 
         // Clear screen
